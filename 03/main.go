9913bf20ca5e72ed0192846f5999dd421aa54c98
Edward Dale <scompt@scompt.com>
2020-12-05 09:15:28 +0100

    Day 03 Part 1

---
+++ 03/main.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+import "strings"
+import "bufio"
+import "os"
+import "io"
+
+type location struct {
+	x int
+	y int
+}
+
+type treeMap struct {
+	width  int
+	height int
+	trees  map[location]struct{}
+}
+
+func (m *treeMap) String() string {
+	builder := strings.Builder{}
+	for y := 0; y < m.height; y++ {
+		for x := 0; x < m.width; x++ {
+			if _, ok := m.trees[location{x, y}]; ok {
+				builder.WriteByte('#')
+			} else {
+				builder.WriteByte('.')
+			}
+		}
+		builder.WriteByte('\n')
+	}
+	return builder.String()
+}
+
+func (m *treeMap) countTrees(right int, down int) int {
+	x := 0
+	y := 0
+	count := 0
+
+	for {
+		if y > m.height {
+			break
+		}
+
+		if _, ok := m.trees[location{x % m.width, y}]; ok {
+			count++
+		}
+
+		x += right
+		y += down
+	}
+
+	return count
+}
+
+func parseTreeMap(input string) (*treeMap, error) {
+	width := -1
+	height := -1
+	trees := make(map[location]struct{})
+
+	row := 0
+	col := -1
+	for i, c := range input {
+		col++
+		if c == '\n' {
+			col = -1
+			row++
+			if width == -1 {
+				width = i
+				height = 1
+			} else {
+				height++
+			}
+		} else if c == '#' {
+			trees[location{col, row}] = struct{}{}
+		}
+	}
+
+	if width == -1 || height == -1 {
+		return nil, fmt.Errorf("%d %d", width, height)
+	}
+	m := treeMap{width, height + 1, trees}
+
+	return &m, nil
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	buf := new(strings.Builder)
+	_, err := io.Copy(buf, reader)
+	if err != nil {
+		panic(err)
+	}
+
+	treeMap, err := parseTreeMap(buf.String())
+	fmt.Printf("%d\n", treeMap.countTrees(3, 1))
+}
---
+++ 03/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import "testing"
+import "github.com/stretchr/testify/assert"
+
+func TestParseBadTreeMap(t *testing.T) {
+	treeMap, err := parseTreeMap("asdf")
+
+	assert.Nil(t, treeMap)
+	assert.Error(t, err)
+}
+
+func TestParseGoodTreeMap(t *testing.T) {
+	input := `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
+	treeMap, err := parseTreeMap(input)
+
+	assert.NotNil(t, treeMap)
+	assert.NoError(t, err)
+	assert.Equal(t, 11, treeMap.width)
+	assert.Equal(t, 11, treeMap.height)
+	assert.NotNil(t, treeMap.trees)
+	assert.Equal(t, 37, len(treeMap.trees))
+}
+
+func TestCountTrees(t *testing.T) {
+	input := `..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
+	treeMap, _ := parseTreeMap(input)
+	count := treeMap.countTrees(3, 1)
+	assert.Equal(t, 7, count)
+}
