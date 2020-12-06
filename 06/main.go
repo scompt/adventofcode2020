3bfdb69b4c87575fe85b62a754b713ebf756f528
Edward Dale <scompt@scompt.com>
2020-12-06 16:47:46 +0100

    Day 06 Part 2

--- 06/main.go
+++ 06/main.go
@@ -9,32 +9,49 @@ import (
 
 type group struct {
 	yesQuestions map[byte]struct{}
+	personCount  int
+	answerCounts map[byte][]int
+}
+
+func (g *group) countUnanimousQuestions() int {
+	count := 0
+	for _, answers := range g.answerCounts {
+		if len(answers) == g.personCount {
+			count++
+		}
+	}
+	return count
 }
 
 func parseGroup(reader *bufio.Reader) (*group, error) {
 	yesQuestions := make(map[byte]struct{})
+	personCount := 0
+	answerCounts := make(map[byte][]int)
 	for {
 		for {
 			b, err := reader.ReadByte()
 			if b == '\n' || b == 0 {
+				personCount++
 				break
 			}
 
 			yesQuestions[b] = struct{}{}
+			answerCounts[b] = append(answerCounts[b], 1)
 			if err == io.EOF {
+				personCount++
 				break
 			}
 		}
 
 		b, err := reader.ReadByte()
 		if b == '\n' {
-			return &group{yesQuestions}, nil
+			return &group{yesQuestions, personCount, answerCounts}, nil
 		}
 		if err != nil {
 			if len(yesQuestions) == 0 {
 				return nil, err
 			}
-			return &group{yesQuestions}, nil
+			return &group{yesQuestions, personCount, answerCounts}, nil
 		}
 
 		err = reader.UnreadByte()
@@ -42,6 +59,25 @@ func parseGroup(reader *bufio.Reader) (*group, error) {
 			return nil, err
 		}
 	}
+}
+
+func countUnanimousQuestions(reader *bufio.Reader) int {
+	groups := []group{}
+
+	for {
+		g, err := parseGroup(reader)
+		if err == nil {
+			groups = append(groups, *g)
+		} else {
+			break
+		}
+	}
+
+	count := 0
+	for _, g := range groups {
+		count += g.countUnanimousQuestions()
+	}
+	return count
 }
 
 func countYesQuestions(reader *bufio.Reader) int {
@@ -64,6 +100,6 @@ func countYesQuestions(reader *bufio.Reader) int {
 }
 
 func main() {
-	count := countYesQuestions(bufio.NewReader(os.Stdin))
+	count := countUnanimousQuestions(bufio.NewReader(os.Stdin))
 	fmt.Printf("%d\n", count)
 }
--- 06/main_test.go
+++ 06/main_test.go
@@ -28,6 +28,29 @@ func TestParseGroup(t *testing.T) {
 	}
 }
 
+func TestCountUnanimousQuestions(t *testing.T) {
+	tests := []struct {
+		input            string
+		personCount      int
+		yesQuestionCount int
+	}{
+		{"abc", 1, 3},
+		{"a\nb\nc", 3, 0},
+		{"ab\nac", 2, 1},
+		{"a\na\na\na", 4, 1},
+		{"b", 1, 1},
+	}
+
+	for _, tt := range tests {
+		reader := bufio.NewReader(strings.NewReader(tt.input))
+		group, err := parseGroup(reader)
+		assert.NoError(t, err, tt.input)
+		assert.NotNil(t, group, tt.input)
+		assert.Equal(t, tt.personCount, group.personCount, tt.input)
+		assert.Equal(t, tt.yesQuestionCount, group.countUnanimousQuestions(), tt.input)
+	}
+}
+
 func TestCountYesQuestions(t *testing.T) {
 	tests := []struct {
 		input            string
@@ -40,5 +63,20 @@ func TestCountYesQuestions(t *testing.T) {
 		reader := bufio.NewReader(strings.NewReader(tt.input))
 		count := countYesQuestions(reader)
 		assert.Equal(t, tt.yesQuestionCount, count)
+	}
+}
+
+func TestCountUnanimousQuestions2(t *testing.T) {
+	tests := []struct {
+		input                  string
+		unanimousQuestionCount int
+	}{
+		{"abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb", 6},
+	}
+
+	for _, tt := range tests {
+		reader := bufio.NewReader(strings.NewReader(tt.input))
+		count := countUnanimousQuestions(reader)
+		assert.Equal(t, tt.unanimousQuestionCount, count)
 	}
 }
