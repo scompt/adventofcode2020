bba795129a4bcafbc06c1a71c5e37e82023f43a7
Edward Dale <scompt@scompt.com>
2020-12-13 21:02:27 +0100

    Day 13 Part 2

--- 13/main.go
+++ 13/main.go
@@ -4,16 +4,50 @@ import "fmt"
 
 func main() {
 
-	start := 1000417
-	nums := []int{23, 41, 37, 479, 13, 17, 29, 373, 19}
-	for _, num := range nums {
+	buses := map[int]int{
+		23:  0,
+		41:  13,
+		37:  17,
+		479: 23,
+		13:  36,
+		17:  40,
+		29:  52,
+		373: 54,
+		19:  73,
+	}
 
-		for i := 0; ; i += num {
-			if i > start {
-				fmt.Printf("%d %d %d %d\n", num, i, i-start, (i-start)*num)
-				break
+	orderedBuses := []int{479, 373, 41, 37, 29, 23, 19, 17, 13}
+
+	busIndex := 0
+	bus := orderedBuses[busIndex]
+	busOffset := buses[bus]
+	longestSequence := 0
+
+	for i := 0; ; {
+		anchor := i - busOffset
+		fmt.Printf("%d\n", anchor)
+		yay, sequenceLength := woot(orderedBuses, buses, anchor)
+		if yay {
+			fmt.Printf("%d\n", anchor)
+			return
+		} else if sequenceLength > longestSequence {
+			longestSequence = sequenceLength
+			bus = 1
+			for j := 0; j < longestSequence; j++ {
+				bus *= orderedBuses[j]
 			}
+		}
+		i += bus
+	}
+}
 
+func woot(orderedBuses []int, buses map[int]int, anchor int) (bool, int) {
+	for i, bus2 := range orderedBuses {
+		if (anchor+buses[bus2])%bus2 != 0 {
+
+			//fmt.Printf("%d\n", bus2)
+			return false, i
 		}
 	}
+	return true, 0
 }
